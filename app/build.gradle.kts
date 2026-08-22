d62c0dd7ea4f59590bec306edf57e8765ea8043b
symbuzzer <alipolatbeyaz@gmail.com>
2026-08-22 14:46:15 +0300

    v1.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 30
         targetSdk = 37
         versionCode = 3
-        versionName = "1.0.2"
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         ndk {
