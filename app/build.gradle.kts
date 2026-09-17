8406b7db32f1f196864f8a2f8efb2d2fb2dd1527
symbuzzer <alipolatbeyaz@gmail.com>
2026-09-17 13:07:23 +0300

    v1.6.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.avalibeyaz.evrak"
         minSdk = 30
         targetSdk = 37
-        versionCode = 32
-        versionName = "1.6.1"
+        versionCode = 33
+        versionName = "1.6.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         ndk {
